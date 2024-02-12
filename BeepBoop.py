67a48eaae8e17f35456f08566bc4df282fc4bd08
Spagitikoch <159771204+Spagitikoch@users.noreply.github.com>
2024-02-12 13:44:24 +0100

    Upload new Version of BeepBoop

--- BeepBoop.py
+++ BeepBoop.py
@@ -1,4 +1,11 @@
-print("I am not a robot, beep boop!")
+print("I am not a robot, beep beep boop!")
 print("I meant to say *human noise*")
 print("Oh no, the programm is changing")
-Changes = 1
+Changes = 2
+description = "Very minor Adjustment"
+
+def Mahlzeit(Essen):
+    for i in Essen:
+        print(f"Es wird {i} gegessen")
+
+Mahlzeit(["Brot","Wasser","Mehl"])
